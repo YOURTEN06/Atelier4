2e5fe4e2c7060fdd6486831b254cce0f9c71c49c
yourten bouchetit <yourten.bouchetit@monboreal.ca>
2026-03-10 16:05:43 -0400

    correction erreur syntaxe print prenom

--- atelier4.py
+++ atelier4.py
@@ -1,12 +1,4 @@
 class Employe:
-    def __init__(self, numeroPermis, nom, prenom):
-        self.numeroPermis = numeroPermis
-        self.nom = nom
-        self.prenom = prenom
-        self.voitureService = None
-
-
-        class Employe:
             def __init__(self, numeroPermis, nom, prenom):
                 self.numeroPermis = numeroPermis
                 self.nom = nom
